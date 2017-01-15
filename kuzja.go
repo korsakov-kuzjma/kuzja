bc200b2dbd0becaf54251100d068b54fb7d1596d
Kuzjma Korsakov <korsakov.kuzjma@gmail.com>
2017-01-16 00:42:24 +0800

    Initial commit

---
+++ kuzja.go
@@ -0,0 +1,12 @@
+package kuzja
+
+import "net/http"
+
+func Wrap(fw ...func(w http.ResponseWriter, r *http.Request)) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		for _, ffw := range fw {
+			ffw(w, r)
+		}
+	}
+}
+
